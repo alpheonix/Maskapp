26191d6780825faa148e6c78309423c52fc4e401
alpheonix <arthurblanc98@gmail.com>
2020-12-15 09:48:36 +0100

    Initial Commit

---
+++ VideoStream/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoStream
+//
+//  Created by Arthur BLANC on 15/12/2020.
+//  Copyright © 2020 Arthur BLANC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
