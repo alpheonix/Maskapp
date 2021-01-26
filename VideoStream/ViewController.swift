3c2f03bb42885d2d2a81e57b01ca092cfea76f47
alpheonix <arthurblanc98@gmail.com>
2021-01-26 16:51:26 +0100

    test

--- VideoStream/ViewController.swift
+++ VideoStream/ViewController.swift
@@ -7,14 +7,44 @@
 //
 
 import UIKit
+import MJPEGStreamLib
 
 class ViewController: UIViewController {
 
+   @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var loadingIndicator: UIActivityIndicatorView!
+
+    var stream: MJPEGStreamLib!
+    var url: URL?
+
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    super.viewDidLoad()
+
+        // Set the ImageView to the stream object
+        stream = MJPEGStreamLib(imageView: imageView)
+        // Start Loading Indicator
+        stream.didStartLoading = { [unowned self] in
+        self.loadingIndicator.startAnimating()
+    }
+        // Stop Loading Indicator
+        stream.didFinishLoading = { [unowned self] in
+        self.loadingIndicator.stopAnimating()
     }
 
+        // Your stream url should be here !
+        let url = URL(string: "http://192.168.1.97:8000/stream.mjpeg")
+        stream.contentURL = url
+        stream.play() // Play the stream
+    }
 
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+    // Make the Status Bar Light/Dark Content for this View
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+        //return UIStatusBarStyle.default   // Make dark again
+    }
 }
 
